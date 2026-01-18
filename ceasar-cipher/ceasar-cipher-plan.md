eaf5ceef4aaa3ee46ce6c969f3f7206d631f1a26
Tùng Nè <lenguyenthanhtung250997@gmail.com>
2026-01-18 23:49:34 +0700

    update ceasar_cipher

---
+++ ceasar-cipher/ceasar-cipher-plan.md
@@ -0,0 +1,5 @@
+1. Receive user message
+2. Split message into array
+3. Compare message array to the alphabets up or down
+4. Map chars in message with cipher_formular
+5. If the chars is not contain in the alphabets, then drop out
---
+++ ceasar-cipher/ceasar-cipher.rb
@@ -0,0 +1,26 @@
+def cipher_formular(index, step)
+    return cipher_index = (index + step) % 26
+end
+
+def ceasar_cipher(message, step)
+    a_downcase = ('a'..'z').to_a
+    a_upcase = ('A'..'Z').to_a
+    cipher = []
+
+    plain = message.split("").to_a
+    plain.each do |char|
+        if a_downcase.include?(char)
+            index = cipher_formular(a_downcase.index(char), step)
+            cipher << a_downcase[index]
+        elsif a_upcase.include?(char)
+            index = cipher_formular(a_upcase.index(char), step)
+            cipher << a_upcase[index] 
+        else
+            cipher.push(char)
+        end
+    end
+
+    return cipher.reduce { |cipher_message, char| cipher_message += char }
+end
+
+print ceasar_cipher("What a string!", 5)

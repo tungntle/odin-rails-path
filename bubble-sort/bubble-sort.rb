1218e4b5b5bb4ee89ae4bd8cb3064d66714ac867
Tùng Nè <lenguyenthanhtung250997@gmail.com>
2026-01-21 14:44:52 +0700

    bubble-sort

---
+++ bubble-sort/bubble-sort.rb
@@ -0,0 +1,20 @@
+unsorted = [4, 3, 78, 2, 0, 2]
+
+def bubble_sort(unsorted)
+  temp = 0
+
+  unsorted.each_with_index do |first, i_first|
+    unsorted.each_with_index do |second, i_second|
+      break if i_second == unsorted.length - 1
+      if first < second
+        temp = unsorted[i_first]
+        unsorted[i_first] = second
+        unsorted[i_second] = temp
+      end
+    end
+  end
+
+  print unsorted
+end
+
+bubble_sort(unsorted)
